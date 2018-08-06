0fd43a2ae23ee8c92ad977096edce978bd994145
Megan Evans <megan.evans.ducky@gmail.com>
2018-08-05 20:39:56 -0600

    added initial inquirer promts--still need to show the total cost of shopping cart purchases.

--- bamazon.js
+++ bamazon.js
@@ -2,19 +2,6 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var Table = require('cli-table');
- 
-// instantiate
-// var table = new Table({
-//     head: ['TH 1 label', 'TH 2 label']
-//   , colWidths: [100, 200]
-// });
-
-// table.push(
-//     ['First value', 'Second value']
-//   , ['First value', 'Second value']
-// );
- 
-console.log(table.toString());
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -28,12 +15,21 @@ connection.connect(function (err) {
     console.log("Connected as id:", connection.threadId);
     start();
 })
+var table = new Table({
+    head: ['ID', 'Name', 'Department', 'Price', 'Quantity'],
+
+});
 
 function start() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock],
+            );
+        }
 
+        console.log(table.toString());
 
         inquirer.prompt([{
             name: "addToCart",
@@ -42,9 +38,44 @@ function start() {
             name: "quantity",
             message: "How many would you like to purchase?"
         }, ]).then(function (answer) {
-            if (answer.quanity < res[answer.addToCart]) {
-                console.log("You have added", res.item_id, "to your cart");
-            }
+            var selectQuant = answer.quantity;
+            var selectItem = answer.addToCart;
+            connection.query("SELECT * FROM products WHERE item_id =?", [selectItem], function (err, res) {
+                if (err) throw err;
+                if (selectQuant < res[0].stock) {
+                    var newStock = res[0].stock -= selectQuant;
+                    console.log("You have added", res[0].product_name, "to your cart");
+                    connection.query('UPDATE products SET stock = ? WHERE item_id = ?', [newStock, selectItem], function (err, res) {
+                        if (err) throw err;
+                    })
+                    inquirer.prompt({
+                        type: 'confirm',
+                        name: "continue",
+                        message: "Would you like to add another item to your cart?"
+                    }).then(function (answer) {
+                        if (answer.continue === true) {
+                            start();
+                        } else {
+                            console.log("Thanks for stopping by!")
+                            connection.end();
+                        }
+                    })
+                } else {
+                    console.log("There is not enough stock on hand to add to your cart.");
+                    inquirer.prompt({
+                        type: 'confirm',
+                        name: "continue",
+                        message: "Would you like to add a different item to your cart?"
+                    }).then(function (answer) {
+                        if (answer.continue === true) {
+                            start();
+                        } else {
+                            console.log("Thanks for stopping by!")
+                            connection.end();
+                        }
+                    })
+                }
+            })
         })
     });
 }
